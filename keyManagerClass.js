56fed8ed5532f67e6dc3f28b29c3cd3591231cd1
John Rucker <JHRucker@outlook.com>
2019-12-03 08:50:05 -0600

    undo last

--- keyManagerClass.js
+++ keyManagerClass.js
@@ -166,20 +166,16 @@ function decryptKey(encryptedKeyBuffer) {
         const params = {
             CiphertextBlob: encryptedKeyBuffer
         };
-        try{
-            kms.decrypt(params, (err, data) => {
-                if (err) {
-                    console.log('Error calling kms.decrypt:');
-                    console.log(err);
-                    reject(err);
-                } else {
-                    resolve(data.Plaintext);
-                };
-            });
-        } catch(err) {
-            console.log('Caught AWS KMS error ' + err);
-            reject('Caught AWS KMS error ' + err);
-        };
+
+        kms.decrypt(params, (err, data) => {
+            if (err) {
+                console.log('Error calling kms.decrypt:');
+                console.log(err);
+                reject(err);
+            } else {
+                resolve(data.Plaintext);
+            };
+        });
     });
 };
 
